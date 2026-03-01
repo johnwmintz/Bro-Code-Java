6b29b1543642b83d4163cece3839652caed9b1f6
John Mintz <john.mintz@gmail.com>
2026-03-01 15:28:07 -0600

    adding if/else practice

--- BroCodeProject2/src/Main.java
+++ BroCodeProject2/src/Main.java
@@ -14,8 +14,18 @@ public class Main {
         System.out.print("Enter your gpa: ");
         double gpa = scanner.nextDouble();
 
+        System.out.print("Are you a student? (true/false): ");
+        boolean isStudent =scanner.nextBoolean();
+
         System.out.println("Hello " + name + " you are " + age + " years old.");
         System.out.println("Your GPA is " + gpa + ".");
+        if(isStudent){
+            System.out.println("You are enrolled as a student.");
+        }
+        else{
+            System.out.println("You are NOT enrolled.");
+        }
+
         scanner.close();
 
     }
