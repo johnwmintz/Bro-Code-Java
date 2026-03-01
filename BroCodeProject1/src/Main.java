8ac8361d6837ac635d8ca0a5f25b7e1a7425ca89
John Mintz <john.mintz@gmail.com>
2026-03-01 13:50:47 -0600

    Pushing what I have completed

---
+++ BroCodeProject1/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main (String[] args) {
+
+        System.out.print("I like eggs! \n");
+        System.out.print("It's really good! \n");
+        System.out.print("Buy me eggs! \n");
+
+        //practice with integers 
+
+        int age = 30;
+        int year = 2026;
+        int quantity = 25;
+
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println(quantity);
+
+        // adding int to strings
+        System.out.println("They are " + age +" years old!");
+        System.out.println("The year is " + year + "!");
+        System.out.println("We have " + quantity +" cars on the lot!");
+
+        //working with doubles and others
+        double price = 24.99;
+        char currency = '$';
+
+        System.out.println(currency + price);
+
+    }
+
+}
