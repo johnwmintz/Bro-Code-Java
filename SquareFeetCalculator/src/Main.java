1c5731dfabcdd9e0a71d2571d514f2d643a4c1b3
John Mintz <john.mintz@gmail.com>
2026-03-01 20:10:14 -0600

    adding square ft calculator

---
+++ SquareFeetCalculator/src/Main.java
@@ -0,0 +1,46 @@
+//This is a basic application on how to calculate square feet
+
+//This imports the utility to allow user input
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main (String[] args) {
+
+        //Initial variable of width height and area
+        double width = 0;
+        double height = 0;
+        double area = 0;
+        String measurement ="sq ft";
+
+        System.out.println("This is a square ft calculator created by John Mintz.  Please enter the measurements in feet.");
+        //This opens the scanner to allow user input
+        Scanner scanner = new Scanner(System.in);
+
+        //This gathers the width and updates the width variable
+        System.out.print("Please enter the width: ");
+        width = scanner.nextDouble();
+
+        //This gathers the height and updates the height variable
+        System.out.print("Please enter the height: ");
+        height =  scanner.nextDouble();
+
+        //Calculating the area and updating the area variable
+        area = width * height;
+
+        //output the area
+        System.out.println("The area is: " + area + " " + measurement);
+
+
+
+
+
+        //This closes the scanner utility
+        scanner.close();
+
+
+
+
+    }
+    
+}
